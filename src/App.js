f168bc3192b445e51c65292ca0f6fb0f7653a531
Julian Kohlman <juliankohlman@gmail.com>
2018-01-08 22:50:51 -0500

    All tests passing, extra features next

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
+// import { render } from 'react-dom';
+
+import TodoList from './components/TodoList';
+
 const App = () => (
   <div>
     <h2>Todo App</h2>
-
+    <TodoList />
   </div>
 );
+
+// render(<App />, document.getElementById('root'));
 
 export default App;
---
+++ src/components/TodoList.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+import Todo from './Todo';
+
+class ToDoList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [],
+      newTodo: ''
+      // [ {text: 'do this', key: 0, style: none}]
+    };
+  }
+
+  handleNewTodo = (event) => {
+    this.setState({ newTodo: event.target.value });
+  };
+
+  addTodo = (event) => {
+    event.preventDefault();
+    const todoList = this.state.todos;
+    todoList.push(this.state.newTodo);
+    this.setState({
+      newTodo: '',
+      todos: todoList
+    });
+  }
+
+  render() {
+    return (
+      <div>
+
+        <form onSubmit={this.addToDo}>
+          <input
+          type="text"
+          onChange={this.handleNewTodo}
+          placeholder="Add a task"
+          value={this.state.newTodo}
+          />
+        </form>
+        {this.state.todos.map((todo, i) => <Todo key={i} item={todo} />)}
+      </div>
+    );
+  }
+}
+
+export default ToDoList;
